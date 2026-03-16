7c3be96cb500347e15f0a56fccb9dd31f91041ba
Ariane ITETERO <a.itetero@alustudent.com>
2026-03-16 09:31:59 +0200

    new changes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,8 @@ const healthRoutes = require("./health.routes.js");
 const donorRoutes = require("./donor.routes.js");
 const hospitalRoutes = require("./hospital.routes.js");
 const requestRoutes = require("./request.routes.js");
-const notificationRoutes = require("./notification.routes");
-const inventoryRoutes    = require("./inventory.routes");
+// const notificationRoutes = require("./notification.routes");
+// const inventoryRoutes    = require("./inventory.routes");
 
 const router = Router();
 
@@ -12,7 +12,7 @@ router.use("/health", healthRoutes);
 router.use("/donors", donorRoutes);
 router.use("/hospitals", hospitalRoutes);
 router.use("/requests", requestRoutes);
-router.use("/notifications", notificationRoutes);
-router.use("/inventory",     inventoryRoutes);
+// router.use("/notifications", notificationRoutes);
+// router.use("/inventory",     inventoryRoutes);
 
 module.exports = router;
